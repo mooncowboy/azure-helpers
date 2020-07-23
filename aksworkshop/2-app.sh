17bb13541993da5aeaa01b049a1c82a52456c682
Ricardo Fiel <rifiel@microsoft.com>
2020-07-24 00:33:55 +0100

    aksworkshop app deploy

--- aksworkshop/2-app.sh
+++ aksworkshop/2-app.sh
@@ -1,19 +1,22 @@
-RESOURCE_GROUP=$1
-ACR_NAME=$2
+# Make sure .env file exists before running this 
+source .env
+
+# set to true to enable MongoDB persistent storage
+PERSISTENCE=false
 
 kubectl create namespace ratingsapp
 
-az acr build \
-    --resource-group $RESOURCE_GROUP \
-    --registry $ACR_NAME \
-    --image ratings-api:v1 \
-    https://github.com/MicrosoftDocs/mslearn-aks-workshop-ratings-api.git
+# az acr build \
+#     --resource-group $RESOURCE_GROUP \
+#     --registry $ACR_NAME \
+#     --image ratings-api:v1 \
+#     https://github.com/MicrosoftDocs/mslearn-aks-workshop-ratings-api.git
 
-az acr build \
-    --resource-group $RESOURCE_GROUP \
-    --registry $ACR_NAME \
-    --image ratings-web:v1 \
-    https://github.com/MicrosoftDocs/mslearn-aks-workshop-ratings-web.git
+# az acr build \
+#     --resource-group $RESOURCE_GROUP \
+#     --registry $ACR_NAME \
+#     --image ratings-web:v1 \
+#     https://github.com/MicrosoftDocs/mslearn-aks-workshop-ratings-web.git
 
 az acr repository list \
     --name $ACR_NAME \
@@ -21,12 +24,12 @@ az acr repository list \
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-$MONGO_USER=ratingsuser
-$MONGO_PASS=ratingspass
+MONGO_USER="ratingsuser"
+MONGO_PASS="ratingspass"
 
-helm install ratings bitnami/mongodb \ 
-    --namespace ratingsapp \ 
-    --set auth.username=$ratingsuser,auth.password=$ratingspass,auth.database=ratingsdb
+helm install ratings bitnami/mongodb \
+    --namespace ratingsapp \
+    --set auth.username=$MONGO_USER,auth.password=$MONGO_PASS,auth.database=ratingsdb,persistence.enabled=$PERSISTENCE
 
 kubectl create secret generic mongosecret \
     --namespace ratingsapp \
@@ -49,5 +52,8 @@ kubectl apply \
 kubectl apply \
     --namespace ratingsapp \
     -f ratings-web-service.yaml
+
+kubectl set image deploy/ratings-api -n ratingsapp ratings-api=$ACR_NAME.azurecr.io/ratings-api:v1
+kubectl set image deploy/ratings-web -n ratingsapp ratings-web=$ACR_NAME.azurecr.io/ratings-web:v1
 
 kubectl get service ratings-web --namespace ratingsapp -w
